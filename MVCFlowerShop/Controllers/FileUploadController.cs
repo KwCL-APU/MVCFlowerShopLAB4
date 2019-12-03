a117d29dc4730e3dc3775b9f72c0dfe704af504f
TP042158 <TP042158@TL06-09-13>
2019-12-03 18:15:43 +0800

    5454

--- MVCFlowerShop/Controllers/FileUploadController.cs
+++ MVCFlowerShop/Controllers/FileUploadController.cs
@@ -23,15 +23,49 @@ namespace MVCFlowerShop.Controllers
             var filepath = Path.GetTempFileName();
             int i = 1; string contents = "";
 
-            foreach(var FormFile in files)
+            foreach (var FormFile in files)
             {
                 //step 1: before pass the file, check the file content type
-                if(FormFile.ContentType.ToLower() != "text/plain")
+                if (FormFile.ContentType.ToLower() != "text/plain")
                 {
                     return BadRequest("The " + Path.GetFileName(filepath) +
                         "is not a text file! Please upload a correct text file");
                 }
+
+                //step 2: chech whether the file is empty anot
+                else if (FormFile.Length <= 0)
+                {
+                    return BadRequest("The " + Path.GetFileName(filepath) +
+                        "is empty!");
+                }
+
+                //step 3: check file size got over limit anot
+                else if (FormFile.Length > 1048576) //more than 1MB
+                {
+                    return BadRequest("The " + Path.GetFileName(filepath) +
+    "is too big!");
+                }
+                //step 4: start to transfer the file to the correct destination
+                else
+                {
+                    //e.g file path
+                    var filedest = "file path" + i + ".txt";
+
+                    using (var stream = new FileStream(filedest, FileMode.Create))
+                    {
+                        await FormFile.CopyToAsync(stream);
+                    }
+
+                    //read file
+                    using (var reader = new StreamReader(FormFile.OpenReadStream()))
+
+                    {
+                        contents = contents + "\\n" + await reader.ReadToEndAsync();
+
+                    }
+                }
             }
+            TempData["message"] = "Success Transfer" + contents;
             return RedirectToAction("Index");
 
         }
