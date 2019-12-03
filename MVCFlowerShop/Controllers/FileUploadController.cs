ae5528783cd718c75b8f28bcdfa8a5afdaba2a93
TP042158 <TP042158@TL06-09-13>
2019-12-03 17:56:33 +0800

    ttt

--- MVCFlowerShop/Controllers/FileUploadController.cs
+++ MVCFlowerShop/Controllers/FileUploadController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -18,6 +19,8 @@ namespace MVCFlowerShop.Controllers
         public async Task<IActionResult> Post (List<IFormFile>files)
         {
 
+            long sizes = files.Sum(f => f.Length);
+            var filepath = Path.GetTempFileName();
             return RedirectToAction("Index");
 
         }
